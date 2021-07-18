175fcd9ab804caa2cfbdcc3cf412df4f7df7d35c
Philip <philip@cldsi.de>
2021-07-18 15:11:42 +0200

    Initial Release 0.8.0
    Initial Release of FT-Bootstrap Theme without Readme, including embedded Google Fonts (non-GDPR compliant), including old mobile-show/hide

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!--
+		====================================================
+		FT Bootstrap Theme by Philip Brembeck 
+		Licensed under the MIT license
+		http://opensource.org/licenses/MIT
+		====================================================
+		Including: 
+		-> Bootstrap: getbootstrap.com
+		-> animate.css: animate.style
+		====================================================
+		-->
+		<title>FT Bootstrap Theme</title>
+		<link href="img/favicon.ico" rel="shortcut icon">
+
+		<!-- begin: Main styles -->
+		<link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<!-- end: Main styles -->
+
+		<!-- begin: Mobile Optimizing -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!-- end: Mobile Optimizing -->
+	</head>
+	<body>
+		<!-- begin: First Screen -->
+		<section id="top" class="cons">
+			<div class="menue animated slideInDown">
+				<a href="#">Link 1</a>
+			</div>
+			<div class="text animated fadeIn">
+				<h2>Title</h2>
+				<h3>Subtitle</h3>
+				<ul class="buttons">
+					<li><a href="#">Learn more</a></li>
+				</ul>
+			</div>
+		</section>
+		<!-- end: First Screen -->
+		
+		<!-- begin: Second Screen -->
+		<section id="mid" class="cons">
+			<div class="text">
+				<h2>Title</h2>
+				<h3>Subtitle</h3>
+				<ul class="buttons">
+					<li><a href="#">Learn more</a></li>
+				</ul>	
+			</div>
+		</section>
+		<!-- end: Second Screen -->
+
+		<!-- begin: Footer -->
+		<footer class="footer">
+			<div class="container">
+				<div class="lefted"><a href="#">FT Bootstraph Theme</a></div>
+				<div class="righted">
+					<a href="https://github.com/jokenetwork">GitHub</a>
+				</div>
+			</div>
+		</footer>
+		<!-- end: Footer -->
+	</body>
+</html>
